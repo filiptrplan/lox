f28f4efacda8f3d64223fb08fe26b6c7ea9e7358
Filip Trplan <filip@trplan.si>
2024-05-22 22:22:28 +0200

    Chapter 7 - Evaluating expressions

--- jlox/src/si/trplan/lox/Lox.java
+++ jlox/src/si/trplan/lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +30,7 @@ public class Lox {
 
         // let the user know there was an error by exiting the program with a return code.
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -49,10 +52,11 @@ public class Lox {
         List<Token> tokens = scanner.scanTokens();
 
         Parser parser = new Parser(tokens);
-        Expr expression = parser.parse(); 
+        Expr expression = parser.parse();
         // Stop if there was a syntax error. 
         if (hadError) return;
-        System.out.println(new AstPrinter().print(expression));
+        
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -65,6 +69,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
